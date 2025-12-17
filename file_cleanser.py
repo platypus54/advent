64724cb0cb26dfcc8109e4f632b4fbf9a28e88c4
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-12-17 12:30:17 -0500

    Add files via upload

---
+++ file_cleanser.py
@@ -0,0 +1,48 @@
+import re
+
+def add_lists(list1=list, list2=list):
+    for x in list2:
+        list1.append(x)
+
+def group_lines(lines=list) -> list:
+
+    grouped_lines = []
+    string_line = str()
+
+    for line in lines:
+        if line != '\n':
+            string_line += line
+        else:
+            grouped_lines.append(string_line)
+            string_line = str()
+    return grouped_lines
+
+def clean_line_1(lines=list) -> tuple:
+    numbers_list = []
+    names = []
+
+    for line in lines:
+        n1 = re.split(r'[:\n]', line)
+        names.append(n1.pop(0))
+        t = [x for x in n1 if x != '']
+        numbers_list.append(t)
+
+    return names,numbers_list
+
+def build_almanac(names=list,numbers=list) -> dict:
+    temp = []
+    almanac = {}
+    name_counter = 0
+
+    for num_str in numbers:
+
+        for item in num_str:
+            n2 = re.split(r' ', item)
+            t = [x for x in n2 if x != '']
+            temp.append(t)
+
+        almanac[names[name_counter]] = temp
+        name_counter += 1
+        temp = []
+
+    return almanac
