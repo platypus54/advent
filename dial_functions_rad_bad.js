439629bf801a23da4a2ccfbf3df29c7337c8c7a4
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-12-18 23:45:51 -0500

    Implement Dial and Combination classes

---
+++ dial_functions_rad_bad.js
@@ -0,0 +1,68 @@
+let TWO_PI = 2 * Math.PI;
+let CLICKS_TO_RADIANS = ( TWO_PI / 100);
+
+
+class Combination {
+
+  constructor(direction, n) {
+    if(direction == 'L')
+      this.isLeft = true;
+    else if (direction == 'R')
+      this.isRight = true;
+
+    this.clicks = n % 100;
+    //this.angle = (CLICKS_TO_RADIANS * this.clicks);
+  }
+}
+
+class Dial
+{
+
+  constructor()
+  {
+    this.start_position = 50; // the starting position of the dial
+    this.position = 0; // the position of the dial as we apply rotations
+    this.rotation_counter = 0; // counter to track the number of rotations we make
+    this.safe_password = 0; // counter to track how many times we land on zero
+  }
+
+
+  move(combo)
+  {
+    this.position = this.start_position;
+
+
+    for (var i = 0; i < combo.length; i++) {
+
+      let tp = 0;
+
+      if(combo[i].isLeft)
+      {
+        tp = (this.position - combo[i].clicks);
+      }
+      else if (combo[i].isRight){
+        tp = (this.position + combo[i].clicks);
+      }
+
+
+      if(tp < 0)
+          this.position = (tp + 100);
+      else
+          this.position = tp % 100;
+
+      // error
+      if(this.position >= 0 && this.position < .001)
+        {this.safe_password++;
+            console.log(this.position, Math.round(this.position))
+        }
+
+      this.rotation_counter++;
+
+      //console.log(this.position , this.rotation_counter, combo[i].clicks);
+  }
+}
+    print_summay()
+    {
+       console.log(this.safe_password , this.rotation_counter);
+    }
+ }
