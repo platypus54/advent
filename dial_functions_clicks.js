61e36e743a66882baf8af2c2eff5c3ce12b216c9
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-12-19 10:47:29 -0500

    Refactor dial movement logic and remove unused code

---
+++ dial_functions_clicks.js
@@ -0,0 +1,57 @@
+class Combination {
+
+  constructor(direction, n) {
+    if(direction == 'L')
+      this.isLeft = true;
+    else if (direction == 'R')
+      this.isRight = true;
+    
+    this.clicks = n % 100;
+  }
+}
+
+class Dial
+{
+
+  constructor()
+  {
+    this.start_position = 50; // the starting position of the dial
+    this.position = 0; // the position of the dial as we apply rotations
+    this.rotation_counter = 0; // counter to track the number of rotations we make
+    this.safe_password = 0; // counter to track how many times we land on zero
+  }
+  
+  move(combo)
+  {
+    this.position = this.start_position;
+
+    for (var i = 0; i < combo.length; i++)
+    {
+        let tp = 0;
+  
+        if(combo[i].isLeft)
+        {
+          tp = (this.position - combo[i].clicks);
+        }
+        else if (combo[i].isRight){
+          tp = (this.position + combo[i].clicks);
+        }
+        
+        if(tp < 0)
+            this.position = (tp + 100);
+        else
+            this.position = tp % 100;
+  
+        if(this.position == 0)
+          this.safe_password++;
+        
+        this.rotation_counter++;
+
+        console.log(this.position , this.rotation_counter, combo[i].clicks);
+   }
+}
+    print_summay()
+    {
+       console.log(this.safe_password , this.rotation_counter);
+    }
+ }
