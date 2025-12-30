db304f82270defe4f5f317484842adb94a7febcc
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-12-30 12:44:48 -0500

    Update ID validation and clean up code
    Refactor ID validation logic and remove unused variables.

--- day2/day2.py
+++ day2/day2.py
@@ -18,20 +18,13 @@ if __name__ == '__main__':
 
     print(numbers)
 
-    str_num = numbers[16] # 11
-    str_num_length = len(str_num)
-
     counter = 0
-    digit = 0
-    current_digit = ''
-    cursor = 0
-
-    print("number",str_num, "length", str_num_length)
-
+    validIDs = []
+    invalidIDs = []
     for x in numbers:
+
         n = len(x)
         if n % 2 == 0:
-
             minl = 0
             maxl = int(n / 2)
             minr = int(n / 2)
@@ -40,10 +33,10 @@ if __name__ == '__main__':
             print(x[minl:maxl], " =  ", x[minr:maxr], " -  ", int(n / 2))
 
             if x[minl:maxl] in x[minr:maxr]:
+                invalidIDs.append(x)
                 counter += 1;
         else:
-            print("validID",x )
-
-
+            validIDs.append(x)
 
     print(counter)
+    print(invalidIDs)
