73d554d0d12d9253dfec9f59438f357ec82e137f
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-12-31 11:28:37 -0500

    Update day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -1,44 +1,51 @@
 import re
 
 if __name__ == '__main__':
-    ranges = []
+    r = []
+
     with open("/../day2TestData.txt") as lines:
         for line in lines:
             n1 = re.split(r',', line)
-            print(n1)
-            for item in n1
-                n2 = re.split(r'-', n1, 1)
-            min = int(n2[0])
+            min = 0
+            max = 0
+            for item in n1:
+                n2 = re.split(r'-', item, 1)
+                #print(n2)
+                min = int(n2[0])
                 max = int(n2[1])
 
-            n = min
-            j = 0
-            temp = []
-            while (n <= max):
-                temp.insert(j,str(n))
-                n+=1
-                j+=1
-            ranges.append(temp)
+                temp = []
+                while (min <= max):
+                    temp.append(str(min))
+                    min+=1
 
-    print(ranges)
-    counter = 0
+                #print(temp)
+                r.append(temp)
+
+    #print(r)
+    sum = 0
     validIDs = []
     invalidIDs = []
-    for x in ranges:
-        for y in x:
-            n = len(y)
-            if n % 2 == 0:
 
+    for x in r:
+        print(x)
+
+        for y in x:
+            print(y)
+            n = len(y)
+
+            if n % 2 == 0:
                 minl = 0
                 maxl = int(n / 2)
                 minr = int(n / 2)
                 maxr = n
                 print(y[minl:maxl], " =  ", y[minr:maxr])
+
                 if y[minl:maxl] in y[minr:maxr]:
                     invalidIDs.append(y)
-                    counter += 1;
-            else:
-                validIDs.append(y)
+                    sum += int(y)
+                else:
+                    validIDs.append(y)
 
-    print(counter == 1227775554)
-    print(invalidIDs)
+
+        print(f'Sum of invalid IDs: {sum} \n InvalidIDs:{invalidIDs} \n ValidIDs:{validIDs} \n')
