428483207f583be93df5d6323e583d58ee111f31
platypus54 <66750619+platypus54@users.noreply.github.com>
2025-12-31 09:30:18 -0500

    Refactor range processing and ID validation logic
    Refactor code to read ranges from a file and process them into lists. Update logic for valid and invalid IDs based on new ranges structure.

--- day2/day2.py
+++ day2/day2.py
@@ -1,42 +1,44 @@
-
+import re
 
 if __name__ == '__main__':
+    ranges = []
+    with open("/../day2TestData.txt") as lines:
+        for line in lines:
+            n1 = re.split(r',', line)
+            print(n1)
+            for item in n1
+                n2 = re.split(r'-', n1, 1)
+            min = int(n2[0])
+                max = int(n2[1])
 
-    r1 = (95, 115)
-    numbers = []
-    invalidIDs = []
+            n = min
+            j = 0
+            temp = []
+            while (n <= max):
+                temp.insert(j,str(n))
+                n+=1
+                j+=1
+            ranges.append(temp)
 
-    min = r1[0]
-    max = r1[1]
-
-    n = min
-    j = 0
-    while (n <= max):
-        numbers.insert(j,str(n))
-        n+=1
-        j+=1
-
-    print(numbers)
-
+    print(ranges)
     counter = 0
     validIDs = []
     invalidIDs = []
-    for x in numbers:
+    for x in ranges:
+        for y in x:
+            n = len(y)
+            if n % 2 == 0:
 
-        n = len(x)
-        if n % 2 == 0:
-            minl = 0
-            maxl = int(n / 2)
-            minr = int(n / 2)
-            maxr = n
+                minl = 0
+                maxl = int(n / 2)
+                minr = int(n / 2)
+                maxr = n
+                print(y[minl:maxl], " =  ", y[minr:maxr])
+                if y[minl:maxl] in y[minr:maxr]:
+                    invalidIDs.append(y)
+                    counter += 1;
+            else:
+                validIDs.append(y)
 
-            print(x[minl:maxl], " =  ", x[minr:maxr], " -  ", int(n / 2))
-
-            if x[minl:maxl] in x[minr:maxr]:
-                invalidIDs.append(x)
-                counter += 1;
-        else:
-            validIDs.append(x)
-
-    print(counter)
+    print(counter == 1227775554)
     print(invalidIDs)
