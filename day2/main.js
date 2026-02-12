6be62c74fb6096e36d328b87ac1cb89972a95e1b
platypus54 <66750619+platypus54@users.noreply.github.com>
2026-02-12 14:58:09 -0500

    Refactor main.js by cleaning up code and adding max
    Removed unused functions and variables, added max and modified console output.

--- day2/main.js
+++ day2/main.js
@@ -1,37 +1,15 @@
 main()
 function main()
 {
-
-  g = generate_num(824824821,824824827)
-  console.log(g)
-  s = 'hello'
-
-  let t = []
-  for (var i = 0; i < g.length; i++) {
-    let k = new Sequence(g[i])
-    t[i] = k
-  }
+  max = 7;
+  t = generate_num(11111,66666)
 
   console.log(t)
+
 }
 
-function find_match(id)
-{
-  if(id[0] == 0)
-    return 0
-
-  n = id.length
-  i = 0
-  k = 0
-  match = ''
-
-  while(i < n)
-    {
-      i++
-      k++
-
-    }
-    return false
+function modMe(n, m){
+  return ((( n % m) + m) % m);
 }
 
 function genNum(n)
@@ -42,7 +20,7 @@ function genNum(n)
   {
     r = k % 10
     k = k / 10
-    t.unshift(Math.floor(r))
+    t.unshift(modMe(Math.floor(r), 7))
   }
   return t;
 }
@@ -57,22 +35,4 @@ function generate_num(i, t)
     numbers.push(genNum(i++))
   }
   return numbers
-}
-
-function find_term(number, m, n){
-
-  if(m > n)
-    return 0
-
-  term = 0
-  while(m < number.length && m <= n)
-  {
-      if(number[m] == 0)
-          break
-
-      term += number[m];
-      m++
-  }
-
-  return term
 }
